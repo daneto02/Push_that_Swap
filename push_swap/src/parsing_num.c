56ebf3f4bc15f4a08e4e7c070d7630be31e01717
David Neto <david.acneto02@gmail.com>
2025-07-22 16:58:00 +0100

    Add files via upload

--- push_swap/src/parsing_num.c
+++ push_swap/src/parsing_num.c
@@ -6,7 +6,7 @@
 /*   By: daneto <daneto@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 11:12:48 by daneto            #+#    #+#             */
-/*   Updated: 2025/07/18 15:13:24 by daneto           ###   ########.fr       */
+/*   Updated: 2025/07/22 14:08:39 by daneto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,21 @@ int check_if_valid(char *str)
 	int i;
 	
 	i = 0;
-	if(str[i] == '-' || str[i] == '+' || str[i] == ' ')
-		str[i]++;
-	if(str[i] == '\0')
-		return(0);
-	while(str[i])
-		{
-			
-		}		
+	while (str[i] == ' ')
+		i++;
+	if ((str[i] == '+' || str[i] == '-') && str[i + 1])
+		i++;
+	if (str[i] == '\0')
+		return (0);
+	while (str[i])
+	{
+		if ((str[i] == '-' || str[i] == '+') && is_num(str[i]) && str[i + 1])
+			i++;
+		else if (str[i] == '\0' || str[i] == ' ')
+			i++;
+		else
+			return (0);
+	}
+	return (1);
 }
+
